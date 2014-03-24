e7951f0b7dad653fc55aa920edec387e51eee3b2
Huynh Tu  Vinh <huynhtuvinh87@gmail.com>
2014-03-24 14:42:01 +0700

    no message

---
+++ apps/models/DbInterface.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asus
+ * Date: 3/21/14
+ * Time: 9:17 AM
+ */ 
---
+++ apps/models/orientDB_Model.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Asus
+ * Date: 3/20/14
+ * Time: 12:08 PM
+ */ 
