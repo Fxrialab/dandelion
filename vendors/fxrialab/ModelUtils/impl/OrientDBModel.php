e0d7a28f6f9ad5c8a4ff8e4e20ded8759983454f
LocBui <minhlocviethanit@gmail.com>
2014-04-21 10:04:58 +0700

    update call gremlin service

--- vendors/fxrialab/ModelUtils/impl/OrientDBModel.php
+++ vendors/fxrialab/ModelUtils/impl/OrientDBModel.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * Created by fxrialab team
- * Author: Uchiha
- * Date: 7/31/13 - 2:18 PM
- * Project: UserWired Network - Version: beta
- * <ul>
- * <li>{Model::get()->find():Model::get()->find("params = ? ", array())}</li>
- * <li>{Model::get()->findAll():Model::get()->findAll}</li>
- * <li>{Model::get()->findByPk():Model::get()->findByPk($id)}</li>
- * <li>{Model::get()->findByAttributes():Model::get()->findByAttributes(array())}</li>
- * <li>{Model::get()->findAllByAttributes():Model::get()->findAllByAttributes(array())}</li>
- * <li>{Model::get()->count()}</li>
- * </ul>
- */
 require_once CONFIG . 'ExtraConfig.php';
 require_once MODELS . "DB.php";
 require_once MODEL_UTILS . "interfaces/IDataModel.php";
@@ -24,8 +10,7 @@ class OrientDBModel implements IDataModel
     protected $_config;
     protected $_className;
     protected $_clusterID;
-    private $flagLoaded = false;
-    public $conditions = '';
+    protected $notUsingHelpers = array('save', 'load', 'find', 'findCustomers', 'update', 'delete');
 
     protected function loadHelpers()
     {
@@ -47,9 +32,19 @@ class OrientDBModel implements IDataModel
         $this->_config = $this->_db->DBOpen(DATABASE, USER, PASSWORD);
         $this->_className = $className;
         $this->_clusterID = ExtraConfig::getId($className);
-        $this->loadHelpers();
+        foreach ($this->notUsingHelpers as $func)
+        {
+            if ($className != $func)
+                $this->loadHelpers();
+        }
     }
 
+    /**
+     * Save a record
+     *
+     * @param array $data
+     * @return string
+     */
     public function save($data)
     {
         $record = new OrientDBRecord();
@@ -68,11 +63,12 @@ class OrientDBModel implements IDataModel
         return $this->_db->recordLoad($id);
     }
 
-    /*
-     * Find a record by condition
-     * @return a object
-     * */
-
+    /**
+     * Find a record by id
+     *
+     * @param string $id
+     * @return mixed
+     */
     public function find($id)
     {
         $sql = "SELECT FROM " . $this->_className ." WHERE @rid = #".$id;
@@ -180,21 +176,24 @@ class OrientDBModel implements IDataModel
         return $queryResult[0]->data->COUNT;
     }
 
-    /*
+    /**
      * Delete record is determined by recordID
-     * @return
-     * */
-
+     *
+     * @param $recordID
+     * @return mixed
+     */
     public function delete($recordID)
     {
         return $this->_db->recordDelete($recordID);
     }
 
-    /*
-     * delete an record was determined by separate conditions with values
-     * @return
-     * */
-
+    /**
+     * Delete an record was determined by separate conditions with values
+     *
+     * @param string $conditions
+     * @param array $values
+     * @return mixed
+     */
     public function deleteByCondition($conditions, $values)
     {
         // @todo: exception for handle parsing error (count("?") != len($values))
@@ -209,6 +208,105 @@ class OrientDBModel implements IDataModel
         return $queryResult;
     }
 
+    /**
+     * @param string $command
+     * @param array|null $arrays
+     * @return array|bool
+     */
+    public function callGremlin($command, $arrays = null)
+    {
+        if (!empty($command))
+        {
+            $sql = "SELECT GREMLIN( '" . $command . "' ) FROM " . $this->_className;
+            if (is_array($arrays) && count($arrays) > 0)
+            {
+                $conditions  = "";
+                $operator   = " AND ";
+                foreach ($arrays as $key => $v) {
+                    $conditions = $conditions.$operator.$key." = ?";
+                    $values[] = $v;
+                }
+                $conditions = substr($conditions,strlen($operator));
+
+                for ($i = 0; $i < count($values); $i++) {
+                    $preparedValue = "'" . $this->SecurityHelper->postIn($values[$i]) . "'";
+                    $conditions = $this->StringHelper->replaceFirst("?", $preparedValue, $conditions);
+                }
+                $sql = $sql . " WHERE " . $conditions;
+            }
+            $queryResult = $this->_db->command(OrientDB::COMMAND_QUERY, $sql);
+
+            if (!empty($queryResult))
+            {
+                $stringResult = $this->getString($queryResult);
+                $result = $this->getContentResult($stringResult, $this->_className);
+                return $result;
+            }else
+                return false;
+        }else {
+            return false;
+        }
+    }
+
+    private function getString($var)
+    {
+        $toFind = "GREMLIN";
+        $array = array();
+        for ($i = 0; $i < count($var); $i++)
+        {
+            $pos[$i] = strpos($var[$i]->content, $toFind) + strlen($toFind);
+            $start[$i] = (int) $pos[$i];
+            $result[$i] = substr($var[$i]->content, $start[$i] + 1, strlen($var[$i]->content) - $start[$i]);
+            array_push($array, $result[$i]);
+        }
+        return $array;
+    }
+
+    private function getContentResult($resultGremlin, $className)
+    {
+        $toFirstFind = 'com.tinkerpop.blueprints.impls.orient.OrientVertex|#';
+        $toSecondFind = '[#';
+        $toThirdFind = '}';
+        $arrayResult = array();
+        for ($i = 0; $i < count($resultGremlin); $i++)
+        {
+            $pos1[$i] = strpos($resultGremlin[$i], $toFirstFind);
+            if (!empty($pos1[$i]))
+            {
+                $replace[$i] = str_replace(array($toFirstFind, ')', '[', ']'), '', $resultGremlin[$i]);
+                $arrayResult = explode(',', $replace[$i]);
+            }else {
+                $pos2[$i] = strpos($resultGremlin[$i], $toSecondFind);
+                if (!empty($pos2[$i]))
+                {
+                    $replace[$i] = str_replace(array('v(' . $className . ')[#', ']'), '', $resultGremlin[$i]);
+                    array_push($arrayResult, $replace[$i]);
+                }else {
+                    $pos3[$i] = strpos($resultGremlin[$i], $toThirdFind);
+                    if (!empty($pos3[$i]))
+                    {
+                        $startPos[$i] = strpos($resultGremlin[$i], '[');
+                        $endPos[$i] = strpos($resultGremlin[$i], ']');
+                        if (!$startPos[$i] && !$endPos[$i]) {
+                            $jsonString[$i] = '[' . $resultGremlin[$i] . ']';
+                            ;
+                        } else {
+                            $jsonString[$i] = $resultGremlin[$i];
+                        }
+
+                        $obj[$i] = json_decode($jsonString[$i]);
+
+                        $arrayResult = $obj[$i];
+                    } else {
+                        $replace[$i] = str_replace(array('"', '[', ']'), '', $resultGremlin[$i]);
+                        $arrayResult = explode(',', $replace[$i]);
+                    }
+                }
+            }
+        }
+
+        return $arrayResult;
+    }
 }
 
 ?>
