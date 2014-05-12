f39359dc7c83a81f45d7d42353290091066ac08e
Huynh Tu  Vinh <huynhtuvinh87@gmail.com>
2014-05-12 11:46:17 +0700

    fix head, footer

--- apps/views/default/layouts/footer.php
+++ apps/views/default/layouts/footer.php
@@ -4,24 +4,24 @@
  * and open the template in the editor.
  */
 ?>
-<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>customs/init.js"></script>
+<!--<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>-->
+
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>infinitescroll.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.uploadfile.min.js"></script>
+<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.selectric.js"></script>
+<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>customs/init.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/holder.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/ink.min.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/ink-ui.min.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/autoload.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/jquery.autosize.min.js"></script>
-<!--<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/blocksit.min.js"></script>-->
+<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/blocksit.min.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/jquery.timers-1.2.js"></script>
-<!--<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>customs/dropDownStyle.js"></script>-->
+<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>customs/dropDownStyle.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.tokeninput.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.leanModal.min.js"></script>
-<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.selectric.js"></script>
+
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.dropdown.js"></script>
-<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>imagesloaded.js"></script>
-<script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>jquery.drag-n-crop.js"></script>
 <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>customs/general.js"></script>
 <script type="text/javascript">
     $("a[rel*=leanModal]").leanModal();
--- apps/views/default/layouts/head.php
+++ apps/views/default/layouts/head.php
@@ -4,16 +4,16 @@
     <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>ink.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>reset.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>main.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>button.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>group.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>token-input-facebook.css" type="text/css" />
-    <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>jquery.dropdown.css" type="text/css" />
+    <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>dropdown.css" type="text/css" />
     <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>jquery-ui.css" type="text/css" />
     <!--[if IE 7 ]>
     <link rel="stylesheet" href="<?php echo $this->f3->get('CSS'); ?>ink-ie7.css" type="text/css" media="screen"
           title="no title">
     <![endif]-->
-    <script type='text/javascript' src='http://code.jquery.com/jquery-1.6.1.min.js'></script>
     <script type="text/javascript" src="<?php echo $this->f3->get('JS'); ?>libs/jquery-1.9.1.min.js"></script>
-<!--    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>-->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
 
 </head>
